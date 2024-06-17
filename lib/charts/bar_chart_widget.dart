5ad463799d9d1f6002995cda111cf83dc5a80dfb
Serofer <vonwylamos@gmail.com>
2024-06-17 22:06:34 +0200

    add month titles and fix top-padding bug

--- lib/charts/bar_chart_widget.dart
+++ lib/charts/bar_chart_widget.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:fin_view/charts/data/bar_data.dart';
+import 'package:fin_view/charts//bar_titles.dart';
 
 
 class BarChartWidget extends StatelessWidget {
+  
+  
   final double barWidth = 30;
   final double barHeight;
   final double groupSpace = 22; // Space between the bars
@@ -12,6 +15,63 @@ class BarChartWidget extends StatelessWidget {
       : barHeight = BarData.barHeight; // Calculate barHeight based on max spent in timeframe
 
   @override
+
+  Widget bottomTitles(double value, TitleMeta meta) {
+    const style = TextStyle(fontSize: 10);
+    String text;
+    switch (value.toInt())  {
+      case 0:
+        text = 'Jan';
+        break;
+      case 1:
+        text = 'Feb';
+        break;
+      case 2:
+        text = 'Mar';
+        break;
+      case 3:
+        text = 'Apr';
+        break;
+      case 4:
+        text = 'May';
+        break;
+      case 5:
+        text = 'Jun';
+        break;
+      case 6:
+        text = 'Jul';
+        break;
+      case 7:
+        text = 'Aug';
+        break;
+      case 8:
+        text = 'Sep';
+        break;
+      case 9:
+        text = 'Oct';
+        break;
+      case 10:
+        text = 'Nov';
+        break;
+      case 11:
+        text = 'Dec';
+        break;
+      default:
+        text = 'lol';
+        break;
+    }
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+       child: Text(text, style: style,)
+      );
+  }
+  Widget topTitles(double value, TitleMeta meta) {
+    const style = TextStyle(fontSize: 10);
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      child: Text('', style: style),
+    );
+  }
   Widget build(BuildContext context) {
     // Calculate total width required for the chart
     // Calculate total width required for the chart
@@ -31,6 +91,23 @@ class BarChartWidget extends StatelessWidget {
             minY: 0,
             groupsSpace: groupSpace,
             barTouchData: BarTouchData(enabled: true),
+            titlesData: FlTitlesData(
+              show: true,
+              bottomTitles: AxisTitles(
+                sideTitles: SideTitles(
+                  showTitles: true,
+                  reservedSize: 28,
+                  getTitlesWidget: bottomTitles,
+                ),
+              ),
+              topTitles:  AxisTitles(
+                    sideTitles: SideTitles(
+                      showTitles: true,
+                      reservedSize: 28,
+                      getTitlesWidget: topTitles,
+                      ),
+                  ), 
+            ),
             /*titlesData: FlTitlesData(
                 show: true,
                 bottomTitles: AxisTitles(
